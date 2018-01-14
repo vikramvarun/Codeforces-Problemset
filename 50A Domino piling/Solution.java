eb51370fedc9f0f15e2232a109a68675291a960c
Vikram Varun <10042572+vikramvarun@users.noreply.github.com>
2018-01-14 16:37:12 +0530

    Create Solution.java

---
+++ 50A Domino piling/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Solution
+{
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+        int result = N*M/2;
+        System.out.print(result);
+        in.close();
+    }
+}
